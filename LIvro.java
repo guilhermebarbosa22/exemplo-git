ae77f1f0bbe127a8dcb50ef5cda6ad1d076d7d19
guilhermebarbosa22 <135286979+guilhermebarbosa22@users.noreply.github.com>
2023-06-06 20:28:52 -0300

    Criação nas classes Livro e usuario

---
+++ LIvro.java
@@ -0,0 +1,18 @@
+public class LIvro {
+    
+    private String titulo;
+    private String preco;
+    public String getTitulo() {
+        return titulo;
+    }
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+    public String getPreco() {
+        return preco;
+    }
+    public void setPreco(String preco) {
+        this.preco = preco;
+    }
+    
+}
