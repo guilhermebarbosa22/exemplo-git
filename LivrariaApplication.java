186b4494559f865cb92ba5cdd46a129b026f9a51
guilhermebarbosa22 <135286979+guilhermebarbosa22@users.noreply.github.com>
2023-06-06 20:15:31 -0300

    LivrariaApplication.java

--- LivrariaApplication.java
+++ LivrariaApplication.java
@@ -1,6 +1,28 @@
+import java.util.Scanner;
+
 public class LivrariaApplication {
 
     public static void main(String[] args) {
+
+       Scanner sc = new Scanner(System.in);
+       
+       System.out.println("Digite seu nome:");
+       String nome = sc.nextLine();
+       System.out.println("Digita idade");
+       String idade = sc.nextLine();
+       sc.close();
+
+       double maior = 18;
+
+       if (maior>18){
+        System.out.println("MAIOR DE IDADE");
+       }
+       if (maior<18){
+        System.out.println("MENOR DE IDADE");
         
+        ;
+       }
+       System.out.println("olá"+nome+"!");
+       System.out.println(maior);
     }
 }
